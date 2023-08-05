673454282016aeeb91fa222a747ac2ca7b9ee814
renishb10 <renishb10@gmail.com>
2023-08-05 16:56:50 +0530

    modal component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AccordionPage from './pages/AccordionPage';
 import DropdownPage from './pages/DropdownPage';
 import ButtonPage from './pages/ButtonPage';
 import Sidebar from './components/Sidebar';
+import ModalPage from './pages/ModalPage';
 
 function App() {
 
@@ -20,6 +21,9 @@ function App() {
         </Route>
         <Route path="/buttons">
           <ButtonPage />
+        </Route>
+        <Route path="/modal">
+          <ModalPage />
         </Route>
       </div>
     </div>
